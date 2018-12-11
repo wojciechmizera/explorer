72fa742f6c7069b9b4471716a3f902968f1f326a
wojciech.mizera <wojciech.mizera@comarch.com>
2018-12-11 19:19:36 +0100

    Preparing web project

---
+++ explorer.web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace explorer.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string login)
+        {
+            // TODO check login in database
+            return RedirectToAction("Index");
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
